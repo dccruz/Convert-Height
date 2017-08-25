68fbb955e5ba80b2a43eaaedd87d4da60c4faa18
DC Cruz <dccoder@icloud.com>
2017-08-24 19:36:18 -0700

    removed code

--- End program Convert Height in Inches, to Feet and Inches/main.cpp
+++ End program Convert Height in Inches, to Feet and Inches/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;  //Command to avoid typing std:: in front of cout and endl commands
 int main( ) {
-    cout << "DC Cruz June 16th 2017"<<endl <<endl;
+    
     int height=0, feet=0, inches=0;
     cout << "What is your height in inches (example 68): " << endl;
     cin >> height;
